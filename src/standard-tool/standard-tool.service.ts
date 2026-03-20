3ff4c50d471d0339320718d8856167600504bc0b
Nunnaphat Rujipoonpong <66160316@go.buu.ac.th>
2026-03-20 10:40:52 +0700

    style: Remove `eslint-disable prettier/prettier` comments from several files.

--- src/standard-tool/standard-tool.service.ts
+++ src/standard-tool/standard-tool.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
